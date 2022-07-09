f3ae41f5f57bc4ecd6c67a336835db44416b78af
Chris Thomas <christopherthomas55@gmail.com>
2022-07-09 17:29:16 -0500

    CT - Los and lots to do here. Added score tracking on bottom, but also added bug stopping today's

--- lib/ABCellHolder.py
+++ lib/ABCellHolder.py
@@ -6,6 +6,10 @@ class ABCellHolder(object):
         self.all_abs = []
         self.parent = parent
         self.json = None
+        # TO do - set to parent num innings
+        self.inning_runs = {x: 0 for x in range(1,30)}
+        self.inning_lob = {x: 0 for x in range(1, 30)}
+        self.current_inning = None
 
         # Pointers to AB cell are stored below......batter is at 0 and bases
         # are 1-3
@@ -52,16 +56,47 @@ class ABCellHolder(object):
             self.bases[start].add_movement(start, end, runner["movement"]["isOut"])
 
         # TODO - I don't think this handles double steals, see above
+
+        #print(self.bases)
         for runner in runners:
             start = destinations.get(runner["movement"]["originBase"], 0)
             end = destinations[runner["movement"]["end"]]
-
             if end == 4 and not runner["movement"]["isOut"]:
                 self.bases[start].add_run()
             else:
                 self.bases[end] = self.bases[start]
-
             self.bases[start] = None
+
+        # num runs
+        return len(runners), len([x for x in runners if destinations[x["movement"]["end"]] == 4])
+
+
+    def handle_inning_totals(self, inning_no, runs, outs, numRunners):
+        self.inning_runs[inning_no] += runs
+        if outs >= 3:
+            self.inning_lob[inning_no] = numRunners - runs - 1
+            #self.parent.add_rect({
+            #       "fill": "white"
+            #   })
+
+
+            #self.parent.add_text
+            # TODO - Lots of constans here...
+            x0 = self.grid_pointer.x0
+            x1 = self.grid_pointer.x1
+            y0 = self.all_abs[8].y0 + 100
+            y1 = self.all_abs[8].y1 + 100
+            text =  "Runs: " + str(self.inning_runs[inning_no]) + "  "
+            text += "LOB: " + str(self.inning_lob[inning_no]) + "\n"
+
+            self.parent._add_label(text, x0, x1, y0, y1)
+
+    def _add_label(self, label, x0, x1, y0, y1):
+        self.add_rect({"fill":"white", "fill-opacity":"0.0", "width":str(x1-x0),
+            "height":str(y1-y0), "stroke":"black", "x":str(x0), "y":str(y0)}
+        )
+
+
 
 
 
@@ -71,6 +106,7 @@ class ABCellHolder(object):
         inning = 1
         looped = 0
         for play in self.json['liveData']['plays']['allPlays']:
+            self.parent.save("img/home_scorebook.svg")
             isTop = self.parent.home_away == "away"
             if play['result']['type'] == 'atBat' and (play['about']['isTopInning'] == isTop):
                 self.all_abs[grid_pointer].add_count(play['count'])
@@ -79,13 +115,16 @@ class ABCellHolder(object):
                     self.all_abs[grid_pointer].add_play(play)
                     # "Add" batter to start of bases
                     self.bases[0] = self.all_abs[grid_pointer]
-                    self.handle_runners(play["runners"])
+                    (numRunners, num_runs) = self.handle_runners(play["runners"])
 
                     if play['about']['hasOut']:
                         outs += 1
 
                     if "double_play" in play['result'].get('eventType').lower():
                         outs += 1
+
+                    self.grid_pointer = self.all_abs[grid_pointer]
+                    self.handle_inning_totals(inning, num_runs, outs, numRunners)
 
                     # TODO - Runs when 3rd out after not counted
                     if outs != 3:
