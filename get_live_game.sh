16047a80e4c9d4cb0b2027c4fa1b92efa69bba3e
Chris <christopherthomas55@gmail.com>
2022-07-10 21:47:19 -0500

    CT - minor running change

--- get_live_game.sh
+++
@@ -1,6 +0,0 @@
-#curl 'https://statsapi.mlb.com/api/v1.1/game/662751/feed/live?language=en' > example_data.json
-
-echo "-----------------------------\nMust manually find game number from MLB site!\n-----------------------------"
-GAME_NUMBER=662351
-curl "https://statsapi.mlb.com/api/v1.1/game/$GAME_NUMBER/feed/live?language=en" > data/live_data.json
-
--- run.sh
+++ run.sh
@@ -1,2 +1,4 @@
-./get_live_game.sh
+echo "-----------------------------\nMust manually find game number from MLB site!\n-----------------------------"
+GAME_NUMBER=663232
+curl "https://statsapi.mlb.com/api/v1.1/game/$GAME_NUMBER/feed/live?language=en" > data/live_data.json
 python3 lib/Scorebook.py
